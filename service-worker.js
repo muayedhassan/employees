2c197f76fd15c7f66ae844010aff5d87d82b1a2e
moh_12 <51693010+muayedhassan@users.noreply.github.com>
2026-09-07 18:01:30 +0300

    Employee Registry R6.6.1 Reports Fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'employee-registry-r6-6-clear-updates-missing-categories-2026.09.07';
+const CACHE_NAME = 'employee-registry-r6-6-1-reports-fix-2026.09.07';
 const APP_SHELL = [
   './',
   './index.html',
