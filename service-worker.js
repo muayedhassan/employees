201cb0978835ebcebacea67acd9213e527bc0461
moh_12 <51693010+muayedhassan@users.noreply.github.com>
2026-09-07 08:30:20 +0300

    Employee Registry R6.1 Search UX Fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'employee-registry-r6-dashboard-reports-2026.09.06';
+const CACHE_NAME = 'employee-registry-r6-1-search-ux-2026.09.07';
 const APP_SHELL = [
   './',
   './index.html',
